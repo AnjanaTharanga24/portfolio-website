fbd4f7c42e90ca68bbae63e99ea96e143a99ce82
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-11-17 00:25:46 +0530

    new update

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/AnjanaTharanga24",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://mysliit-my.sharepoint.com/my?id=%2Fpersonal%2Fit21826436%5Fmy%5Fsliit%5Flk%2FDocuments%2Fmy%2Dcv%2FAnjana%20tharanga%20cv%2Epdf&parent=%2Fpersonal%2Fit21826436%5Fmy%5Fsliit%5Flk%2FDocuments%2Fmy%2Dcv",
   linkedin: "https://www.linkedin.com/in/anjana-tharanga-99709228b/",
   twitter: "",
   insta: "",
